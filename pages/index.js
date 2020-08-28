8bd241c9ffb0fb798b7baeaaad30d6c025ed0d2b
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2020-08-28 11:54:16 -0600

    Add remaining metrics and some basic styling to match figma designs

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export class Index extends React.Component {
       api_url: process.env.API_URL,
       data: "",
       notifications: {},
+      notifications_by_month: []
     }
   }
 
@@ -16,6 +17,36 @@ export class Index extends React.Component {
     // make data requests
     this.setState({data: await this.fetchData("")});
     this.setState({notifications: await this.fetchData("notifications-by-type")});
+    
+    // construct table array
+    let notifications_by_month_data = await this.fetchData("notifications-by-month-and-type");
+    let notifications_by_month_array = [];
+    for (let year in notifications_by_month_data) {
+      for (let m in notifications_by_month_data[year]) {
+        let month = notifications_by_month_data[year][m];
+        month.date = {"year": year, "month": this.monthNumberToWord(m)}
+        notifications_by_month_array.unshift(month)
+      }
+    }
+    this.setState({notifications_by_month: notifications_by_month_array});
+  }
+
+  monthNumberToWord(m) {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ]
+    return(months[m-1])
   }
 
   async fetchData(url) {
@@ -30,7 +61,7 @@ export class Index extends React.Component {
         }).then(data => {
           return data;
         })
-      console.log(resp_data)
+      // console.log(resp_data)
       return(resp_data)
     } catch(err) {
       console.log(err);
@@ -38,25 +69,83 @@ export class Index extends React.Component {
   }
 
   render() {
-    const Header = styled.h1`
-      font-family: sans-serif;
+    const FlexContainer = styled.div`
+      display: flex;
+      gap: 17px;
+      margin-top: 17px;
+    `;
+
+    const GreyBox = styled.div`
+      background-color: #eee;
+      width: 50%;
+      padding: 1em;
+      margin-bottom: 1em;
+    `;
+
+    const LargeText = styled.div`
+      font-size: 90px;
+    `;
+
+    const MediumText = styled.div`
+      font-size: 60px;
+    `;
+
+    const MediumStats = styled.div`
+      width: 30%;
     `;
 
     return (
-      <div>
+      <div id="main">
         <Head>
-          <title>Notification Metrics Dashboard</title>
+          <title>GC Notify Activity Dashboard</title>
         </Head>
-        <Header>Notification Metrics Dashboard</Header>
-        <div>
-          <div>Notifications sent: {this.state.data.notifications}</div>
-          <div>Services: {this.state.data.services}</div>
-        </div>
-        <Header>Notifications by Type:</Header>
-        <div>
-          <div>Emails sent: {this.state.notifications.email}</div>
-          <div>SMS sent: {this.state.notifications.sms}</div>
-        </div>
+        <h1>Activity Dashboard</h1>
+        <div>GC Notify is helping a growing number of government teams keep their clients informed. The following dashboard highlights its activity since November 2019.</div>
+        <FlexContainer>
+          <GreyBox>
+            <div><strong>Total notifications sent</strong></div>
+            <LargeText>{this.state.notifications.email + this.state.notifications.sms}</LargeText>
+            <div>This includes status updates, appointment reminders, and password resets sent by email and text message.</div>
+          </GreyBox>
+          <GreyBox>
+            <div><strong>Programs and services using GC Notify</strong></div>
+            <LargeText>{this.state.data.services}</LargeText>
+            <div>This includes services offered by federal departments, and provinces and territories.</div>
+          </GreyBox>
+        </FlexContainer>
+        <FlexContainer>
+          <MediumStats>
+            <div>Emails</div>
+            <MediumText>{this.state.notifications.email}</MediumText>
+          </MediumStats>
+          <MediumStats>
+            <div>Text Messages</div>
+            <MediumText>{this.state.notifications.sms}</MediumText>
+          </MediumStats>
+        </FlexContainer>
+        <h2>Notifications sent over time</h2>
+        <table width='100%'>
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>Total notifications sent</th>
+              <th>Emails sent</th>
+              <th>Text Messages sent</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.notifications_by_month.map(function(item, key) {
+              return (
+                <tr key={key}>
+                  <td>{item.date.month} {item.date.year}</td>
+                  <td>{item.email + item.sms}</td>
+                  <td>{item.email}</td>
+                  <td>{item.sms}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     )
   }
